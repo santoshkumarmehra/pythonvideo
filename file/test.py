027d9fd358d13a1bcc4d5dc45afae8bf93263ad0
santosh kumar mehra <santoshkumarmehra99@gmail.com>
2022-09-30 12:32:44 +0530

    first commit

---
+++ file/test.py
@@ -0,0 +1,12 @@
+from pytube import YouTube
+
+yt = YouTube('https://www.youtube.com/watch?v=NvuQ9Eskl04&ab_channel=RaagBandMusic')
+yt.streams.filter(file_extension='mp4')
+
+stream = yt.streams.get_by_itag(22)
+stream.download(filename='ytvideo',output_path='/home/codetrade')
+
+
+
+
+
